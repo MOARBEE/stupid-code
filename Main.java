c0d3d20ae0856c1959228f5d009e1345ccdad5aa
Wolfman13 <34652519+Wolfman13@users.noreply.github.com>
2019-12-27 09:57:45 +0200

    Modified Test Files
    This code should work now

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class Main {
+    final int SETPASS = 7869;
+    int password;
+    int counter = 0;
+    String answer;
+    int newcounter =0;
+    Scanner scanner = new Scanner(System.in);
+
+    public void getpassword() {
+
+        System.out.println("what is your password?: ");
+        password = scanner.nextInt();
+
+        while (password != SETPASS && counter < 2) {
+            System.out.println("Your password is incorrect, please try again: ");
+            password = scanner.nextInt();
+            counter++;
+        }
+
+        if (password != SETPASS || counter == 3) {
+            System.out.println("You are out of tries, would you like to try again? ");
+            answer = scanner.next();
+
+            if (answer.equals("yes")) {
+                System.out.println("what is your password?: ");
+                password = scanner.nextInt();
+                while (password != SETPASS && newcounter < 2) {
+                    System.out.println("Your password is incorrect, please try again: ");
+                    password = scanner.nextInt();
+                    newcounter++;
+                }
+            }
+            else if (password == SETPASS && newcounter <= 3) {
+                System.out.println("Welcome");
+
+            }
+            else if (answer == "no") {
+                System.out.println("goodbye");
+            }
+        }
+
+        if (password == SETPASS && newcounter <= 3) {
+            System.out.println("Welcome");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.getpassword();
+    }
+}
